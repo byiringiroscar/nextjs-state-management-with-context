a32eac5e87fe9bd7e6260d65f3a27bd0e4879516
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-09-24 23:13:59 +0200

    Add and setup store, context and provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { PokemonProvider } from '@/src/store';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <PokemonProvider pokemon={  pageProps.pokemon  }>
+          <Component {...pageProps} />
+        </PokemonProvider>
 }
---
+++ src/store.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useMemo, createContext, useContext } from 'react';
+
+
+interface Pokemon {
+  id: number,
+  name: string,
+  image: string,
+}
+
+export const getServerSideProps = async() => {
+  const resp = await fetch("https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json");
+  const data = await resp.json();
+  return {
+    props: {
+      pokemon: data
+    }
+  }
+}
+
+const usePokemonController = (pokemon: Pokemon[]) => {
+    const [filter, setFilter] = useState("")
+    const filteredPokemon = useMemo(
+        () => pokemon.filter((p) => p.name.toLowerCase().includes(filter.toLowerCase())),
+        [ filter, pokemon ]
+    );
+    return { 
+        filter, 
+        setFilter, 
+        pokemon: filteredPokemon 
+    };
+}
+
+
+const PokemonContext = createContext<ReturnType<typeof usePokemonController>>({
+    filter: "",
+    setFilter: () => {},
+    pokemon: []
+    })
+
+    export const PokemonProvider: React.FC<{ pokemon: Pokemon[]; children: React.ReactNode }> = (
+      { pokemon, children }
+    ) => (
+      <PokemonContext.Provider value={usePokemonController(pokemon)}>
+        {children}
+      </PokemonContext.Provider>
+    );
+    
+
+export const usePokemon = () => useContext(PokemonContext);
